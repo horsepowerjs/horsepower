ee8d250119c9e6b9acbf4615a99e3b516f8227c7
Ryan <untuned20@gmail.com>
2018-09-25 09:07:52 -0500

    stuff

--- red5-test/config/app.js
+++ red5-test/config/app.js
@@ -1,8 +1,8 @@
 const { env } = require('red5')
 
-/** @type {import('red5').AppSettings} */
+/** @type {import('red5').AppSettings} exports */
 module.exports = {
-  port: 500,
+  // port: 5000,
   name: env('APP_NAME', 'Red5'),
   env: env('APP_ENV', 'production'),
   https: false
--- red5-test/index.js
+++ red5-test/index.js
@@ -4,7 +4,7 @@ const { start, stop } = require('red5')
 start()
 
 // Gracefully stop the server
-process.on('SIGTERM', () => stop())
-process.on('SIGINT', () => stop())
-process.on('SIGKILL', () => stop())
-process.on('message', (msg) => msg == 'shutdown' && stop())
+// process.on('SIGTERM', () => stop())
+// process.on('SIGINT', () => stop())
+// process.on('SIGKILL', () => stop())
+// process.on('message', (msg) => msg == 'shutdown' && stop())
