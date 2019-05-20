e60b1734c6e5a76e5546539a962615e421b7b9c5
Ryan Naddy <untuned20@gmail.com>
2019-05-20 08:15:22 -0500

    publish

--- publish.sh
+++ publish.sh
@@ -6,22 +6,21 @@ cd $SCRIPT_DIR
 
 TYPE=""
 
-if [ $1 = "patch" ]; then
-  TYPE="patch"
+if [ $1 = "major" ]; then
+  TYPE="major"
 elif [ $1 = "minor" ]; then
   TYPE="minor"
-elif [ $1 = "major" ]; then
-  TYPE="major"
+elif [ $1 = "patch" ]; then
+  TYPE="patch"
 else
   echo "Invalid option, accepted values are 'major', 'minor' or 'patch'"
   exit
 fi
 
-# cd ./middleware && npm version patch && npm publish --access public &
-# cd ../mysql && npm version patch && npm publish --access public &
-# cd ../router && npm version patch && npm publish --access public &
-# cd ../server && npm version patch && npm publish --access public &
-# cd ../session && npm version patch && npm publish --access public &
-# cd ../storage && npm version patch && npm publish --access public &
-# cd ../template && npm version patch && npm publish --access public &
-wait
+cd ./middleware && npm version "$TYPE" && npm publish --access public
+cd ../mysql && npm version "$TYPE" && npm publish --access public
+cd ../router && npm version "$TYPE" && npm publish --access public
+cd ../server && npm version "$TYPE" && npm publish --access public
+cd ../session && npm version "$TYPE" && npm publish --access public
+cd ../storage && npm version "$TYPE" && npm publish --access public
+cd ../template && npm version "$TYPE" && npm publish --access public
