6b4386ae9c44410e0f3860bc5adb216769fcb777
Ryan Naddy <rnaddy@corp.acesse.com>
2019-08-19 15:41:31 -0500

    Fixed typeo

--- config/app.js
+++ config/app.js
@@ -9,7 +9,7 @@ module.exports = {
   port: parseInt(env('APP_PORT', '5000')),
 
   // This the name of the application.
-  name: env('APP_NAME', 'Horse Power'),
+  name: env('APP_NAME', 'Horsepower'),
 
   // This is the default locale for the application.
   // If the user has never been to the site before,
